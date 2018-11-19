78bf6af40625c13c61f42bcd6c07afaa8e59c71b
Lucas Galvão Nunes <lucasgalvaonu@gmail.com>
2018-11-19 21:39:54 -0200

    Coars middleware for api routes

--- routes/api.php
+++ routes/api.php
@@ -31,38 +31,38 @@ Route::namespace('API\v1')->prefix('v1')->name('api.v1.')->group(function(){
         Route::post('login', 'UsersController@login');
         Route::post('create', 'UsersController@create');
 
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::get('profile', 'UsersController@profile');
             Route::post('update', 'UsersController@update');
         });
     });
 
     Route::prefix('consoles')->group(function () {
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::get('all', 'ConsolesController@all');
         });
     });
 
     Route::prefix('categorys')->group(function () {
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::get('ofConsole/{idConsole}', 'CategorysController@allCategorysOfConsole');
         });
     });
 
     Route::prefix('games')->group(function () {
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::post('ofConsole', 'GamesController@gamesOfConsole');
         });
     });
 
     Route::prefix('steps')->group(function () {
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::get('stepsOfGame/{idGames}/{idStep?}', 'StepsController@stepsOfGame');
         });
     });
 
     Route::prefix('guides')->group(function () {
-        Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['middleware' => ['auth:api', 'cors']], function () {
             Route::get('guideOfStep/{idStep}', 'GuidesController@guideOfStep');
         });
     });
